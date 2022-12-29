bf5ac59f8ffe2ee75733257176726c8b9cf5b4b0
mqq-marek <marek@kubowicz.pl>
2022-12-29 08:16:39 +0100

    Rook-Wieze

--- tests/test_wildtowers.py
+++ tests/test_wildtowers.py
@@ -1,47 +1,128 @@
-
-
-
 import math
+from io import StringIO
 
-from vlo.wildtowers import wild_towers
+from vlo.wildtowers import wild_towers, process_data, make_matrix_from_0_and_1, make_matrix_from_dot_and_letter_o, \
+    generate_combinations, solve_brute_force, allowable_positions, validate_combination
+
+
+def test_can_make_matrix_from_0_1():
+    assert make_matrix_from_0_and_1('') == []
+    assert make_matrix_from_0_and_1('Hello') == []
+    assert make_matrix_from_0_and_1('0') == [[0]]
+    assert make_matrix_from_0_and_1('1') == [[1]]
+    assert make_matrix_from_0_and_1('0110') == [[0, 1], [1, 0]]
+    assert make_matrix_from_0_and_1("""0
+    1 end of the first row
+    1
+    0 end of the second / final row
+    """) == [[0, 1], [1, 0]]
+
+
+def test_can_make_matrix_from_dot_o():
+    assert make_matrix_from_dot_and_letter_o('') == []
+    assert make_matrix_from_dot_and_letter_o('Hello') == [[0]]
+    assert make_matrix_from_dot_and_letter_o('o') == [[0]]
+    assert make_matrix_from_dot_and_letter_o('.') == [[1]]
+    assert make_matrix_from_dot_and_letter_o('o..o') == [[0, 1], [1, 0]]
+
+
+def test_can_solve_brute_force():
+    assert solve_brute_force([]) == 0
+
+
+def test_can_validate_combinations():
+    assert not validate_combination(((0, 0), (1, 0), (2, 1)))
+    assert validate_combination(((0, 0), (1, 1), (2, 2)))
+
+
+def test_can_generate_combinations():
+    assert list(generate_combinations([])) == []
+    assert list(generate_combinations([[0]])) == []
+    assert list(generate_combinations([[1]])) == [((0, 0),)]
+    assert list(generate_combinations([[1, 1], [0, 1]])) == [((0, 0), (1, 1)),
+                                                             ((0, 1), (1, 1))]
+    assert list(generate_combinations([[1, 1, 0], [1, 0, 1], [0, 1, 1]])) == [
+        ((0, 0), (1, 0), (2, 1)),
+        ((0, 0), (1, 0), (2, 2)),
+        ((0, 0), (1, 2), (2, 1)),
+        ((0, 0), (1, 2), (2, 2)),
+        ((0, 1), (1, 0), (2, 1)),
+        ((0, 1), (1, 0), (2, 2)),
+        ((0, 1), (1, 2), (2, 1)),
+        ((0, 1), (1, 2), (2, 2))]
+    assert list(generate_combinations([[1, 1], [0, 0]])) == []
+
+
+def test_can_get_allowable_positions():
+    assert list(allowable_positions([])) == []
+    assert list(allowable_positions([[0]])) == [[]]
+    assert list(allowable_positions([[1]])) == [[(0, 0)]]
+    assert list(allowable_positions([[1, 0], [0, 1]])) == [[(0, 0)], [(1, 1)]]
+    assert list(allowable_positions([[1, 1], [0, 1]])) == [[(0, 0), (0, 1)], [(1, 1)]]
 
 
 def test_can_solve_empty():
     assert wild_towers() == 0
 
+
 def test_can_solve_1x1_square():
     assert wild_towers('1') == 1
+
 
 def test_can_solve_1x1_square_with_hole():
     assert wild_towers('0') == 0
 
+
 def test_can_solve_2x2_square():
     assert wild_towers('1111') == 2
+
 
 def test_can_solve_2x2_square_with_row_holes():
     assert wild_towers('0011') == 0
 
+
 def test_can_solve_empty_squares():
     for i in range(1, 10):
-        assert wild_towers('0'*(i * i)) == 0
+        assert wild_towers('0 ' * (i * i)) == 0
+
 
 def test_can_solve_half_empty_squares():
     print()
-    for i in range(1, 21):
+    for size in range(1, 11):
         board0 = ''
         board1 = ''
-        for j in range(i):
-            if j % 2:
-                gen0 = ('01' * i)[:i]
-                gen1 = ('10' * i)[:i]
+        for position in range(size):
+            if position % 2:
+                gen0 = ('01' * size)[:size]
+                gen1 = ('10' * size)[:size]
             else:
-                gen0 = ('10' * i)[:i]
-                gen1 = ('01' * i)[:i]
+                gen0 = ('10' * size)[:size]
+                gen1 = ('01' * size)[:size]
             board0 += gen0
             board1 += gen1
         print(len(board0), wild_towers(board0), wild_towers(board1))
 
+
 def test_can_solve_full_squares():
-    for i in range(1, 10):
+    for i in range(1, 9):
         assert wild_towers('1' * (i * i)) == math.factorial(i)
 
+def test_can_solve_5x5_squares():
+    print()
+    size = 6
+    assert wild_towers('1' * (size*size), print_result=True) == math.factorial(size)
+
+
+
+def test_can_process_data():
+    input = """1
+3
+..o
+.o.
+o..    
+    """
+    out = StringIO()
+    inp = StringIO(input)
+    process_data(inp, out)
+    text = out.getvalue()[:-1]
+    assert text == '2'
--- vlo/wildtowers.py
+++ vlo/wildtowers.py
@@ -1,12 +1,18 @@
+import itertools
+import json
 import math
+from collections import defaultdict
+from operator import itemgetter
+from sys import stdin, stdout
 
 
-def make_matrix(board):
+def make_matrix_from_0_and_1(board):
     """Read board as a sequence of chars 0 and 1 and treat them as board.
     Char 0 means hole and placing rock is not possible.
     Char 1 means normal place when we can place rook/tower
     Character other than 0 and 1 are skipped.
-    Result is matrix of rows with 0/1 char.
+    Amount of 0 and 1 chars must be square of integer (must form matrix)
+    Result is matrix of rows with 0/1 int.
     """
     assert isinstance(board, str)
     filtered_board = [ch for ch in board if ch in "01"]
@@ -15,19 +21,86 @@ def make_matrix(board):
     assert size * size == total
     rows = [filtered_board[row * size:(row + 1) * size]
             for row in range(size)]
-    matrix = [[ch for ch in row]
+    matrix = [[int(ch) for ch in row]
               for row in rows
               ]
     return matrix
 
 
+def make_matrix_from_dot_and_letter_o(board):
+    """Read board as a sequence of chars 0 and 1 and treat them as board.
+    Char o means hole and placing rock is not possible.
+    Char . means normal place when we can place rook/tower
+    Character other than . and o are skipped.
+    Amount of . and o chars must be square of integer (must form matrix)
+    Result is matrix of rows with 0/1 char.
+    """
+    assert isinstance(board, str)
+    zero_one_board = board.replace('.', '1').replace('o', '0')
+    return make_matrix_from_0_and_1(zero_one_board)
+
+
+def solve_brute_force(matrix):
+    """Solve by creating all possible figure combinations - once at every row - and
+    validate if they all have coordinates which prevent rooks to be able to attack
+    other rook in single move."""
+    list_for_check = generate_combinations(matrix)
+    result = sum(validate_combination(postions) for postions in list_for_check)
+    return result
+
+
+def generate_combinations(matrix):
+    """Create all possible (brute-force) combination list where figures can be placed on board.
+    EG. for matrix 11/01 create [((0, 0), (1, 1)), ((0, 1), (1, 1))].
+
+    """
+    # return itertools.combinations(range(len(matrix)), len(matrix))
+    # return itertools.permutations(range(len(matrix)), len(matrix))
+    return itertools.product(*allowable_positions(matrix))
+
+
+def allowable_positions(matrix):
+    """Based on board matrix generate for list of row list with all coordinates for squares when rook can be placed.
+    EG. for matrix 11/01 create [[(0, 0), (0, 1)], [(1, 1)]]. """
+    if matrix:
+        positions = [
+            [(row_ndx, col_ndx)
+             for col_ndx, square in enumerate(row)
+             if square == 1
+             ]
+            for row_ndx, row in enumerate(matrix)
+        ]
+        return positions
+    return [[]]
+
+
+def validate_combination(positions):
+    """Validate if given list of fugures postions is ok for rook that means that
+    all x and all y coordinates are different."""
+    x_coords, y_coords = zip(*positions)
+    return len(set(x_coords)) == len(set(y_coords)) == len(positions)
+
+
+def process_data(input, output):
+    z = int(input.readline())
+    for _ in range(z):
+        n = int(input.readline())
+        board = ''
+        for _ in range(n):
+            board += input.readline()
+        matrix = make_matrix_from_dot_and_letter_o(board)
+        result = wild_towers(matrix)
+        output.write(str(result) + '\n')
+
+
 class TowerSolver:
 
-    def __init__(self, board):
+    def __init__(self, matrix):
         # Convert board to matrix
-        self.matrix = make_matrix(board)
+        self.matrix = matrix
         # Make empty hash table/dictionary for storing temporary results
         self.mapper = {}
+        self.counter = defaultdict(int)
 
     def solver(self):
         return self.solve([ndx for ndx in range(len(self.matrix))])
@@ -35,12 +108,14 @@ class TowerSolver:
     def solve(self, columns):
         # list of columns determine matrix size for solve - at start we have all columns - full matrix size
         size = len(columns)
+        # For every matrix for solve keep its result under key <col1>_<col2>_..._<coln>
+        map_key = '_'.join(map(str, columns))
+        self.counter[map_key] += 1
         if size == 0:
             return 0
         if size == 1:
             return int(self.matrix[0][columns[0]])
-        # For every matrix for solve keep its result under key <col1>_<col2>_..._<coln>
-        map_key = '_'.join(map(str, columns))
+
         result = self.mapper.get(map_key)
         if result is None:
             # cCompute result if not in mapper
@@ -67,12 +142,32 @@ def solve_loop_recursive(matrix):
                 ])
 
 
-def wild_towers(board=''):
-    matrix = make_matrix(board)
-    # r1 = solve_loop_recursive(matrix)
-    t = TowerSolver(board)
-    r2 = t.solver()
-    r1 = r2
-    if r1 != r2:
-        print(f'Fail solve {matrix} and get {r1} & {r2}')
-    return r1
+DEBUG = True
+
+
+def wild_towers(board='', print_result=False):
+    matrix = []
+    if isinstance(board, str):
+        if '0' in board or '1' in board:
+            matrix = make_matrix_from_0_and_1(board)
+        else:
+            matrix = make_matrix_from_dot_and_letter_o(board)
+    elif isinstance(board, list):
+        matrix = board
+
+    t = TowerSolver(matrix)
+    result = t.solver()
+    if print_result:
+        keys = sorted(t.counter, key=t.counter.get, reverse=False)
+        for key in keys:
+            print(f"{key}:{t.counter[key]}")
+    if DEBUG:
+        result_first = solve_brute_force((matrix))
+        result_test = solve_loop_recursive(matrix)
+        if not result == result_test == result_first:
+            print(f'Fail solving {matrix} and get {result} & {result_test} & {result_first}')
+    return result
+
+
+if __name__ == "__main__":
+    process_data(stdin, stdout)
